934beeef27e91ffd77b97d8d57bbe05fdd29f5a9
Cameron Brown <cameron-brown@live.com>
2020-11-03 22:11:07 +0000

    fixed linting

--- timer1.js
+++ timer1.js
@@ -2,15 +2,15 @@ const setTimer = (alarm) => {
   setTimeout(() => {
     process.stdout.write('.   ');
   }, alarm * 1000);
-}
+};
 
 const timer = () => {
   let alarms = process.argv.slice(2);
   for (let number of alarms) {
-    if (number > 0 && number !== NaN) {
-      setTimer(number)
+    if (number > 0 && !isNaN(number)) {
+      setTimer(number);
     }
   }
 
-}
+};
 timer();
